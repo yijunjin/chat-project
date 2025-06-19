ad7bc44e9d590d88c3ebd8d9738f310e5ad1c545
易军进 <929713449@qq.com>
2025-06-19 23:51:09 +0800

    feat: 搭建前后端仓库

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  plugins: [vue()],
+});
--- packages/frontend/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { isObject } from "@chat/shared";
-import { test } from "@chat/backend";
-console.log(isObject({}));
-console.log(test());
